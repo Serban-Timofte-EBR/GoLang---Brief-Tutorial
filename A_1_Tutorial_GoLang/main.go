de5358c9b401983cb2db24ad360e3af97ab87063
Serban Timofte <serbantimofte@192.168.1.136>
2024-07-17 22:47:35 +0300

    Initial commit

---
+++ A_1_Tutorial_GoLang/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	// Variable Declaration
+	var x int = 10
+	fmt.Println(x)
+
+	y := 20
+	fmt.Println(y)
+
+	const pi = 3.14
+	fmt.Println("Value of pi is", pi)
+}
